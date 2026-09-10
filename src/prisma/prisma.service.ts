61cff1f1f54220958dfc65d0312e97a711dfe130
JJV <juanjovargasm1@mail.com>
2026-09-11 00:14:23 +0200

    Configurar Prisma 7 y dependencias compatibles con NestJS 10

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 // La importación ahora apunta a la salida personalizada que definimos en schema.prisma
-import { PrismaClient } from '../generated/prisma/client.js'; 
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    const connectionString = process.env.DATABASE_URL; // Esta es la POOLED
-    const pool = new Pool({ connectionString });
+      const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
     const adapter = new PrismaPg(pool);
 
     super({ adapter });
